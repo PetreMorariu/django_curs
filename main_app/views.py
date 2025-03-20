015a1e4f0c4ff2751cdfcdbcf73145ba6232ff8d
pmorariu <morariupetru@yahoo.com>
2025-03-20 19:11:27 +0200

    third commit

--- main_app/views.py
+++ main_app/views.py
@@ -127,23 +127,6 @@ def get_books(request: HttpRequest):
             return HttpResponse("Wrong ID given!", status=400)
 
 
-
-# test
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
